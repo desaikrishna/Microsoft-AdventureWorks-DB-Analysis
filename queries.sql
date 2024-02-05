f99f8cc403a893247a833c9423a83754149d3026
desaikrishna <desaikrishna007@gmail.com>
2024-02-05 23:58:14 +0530

    more queries and analysis

--- queries.sql
+++ queries.sql
@@ -175,6 +175,98 @@ on p.businessEntityId = b.personid
 'Check why the above query is not working'
 
 
+'21. From the following tables write a query in SQL to retrieve the salesperson for each PostalCode who belongs to a territory and SalesYTD is not zero. 
+Return row numbers of each group of PostalCode, last name, salesytd, postalcode column. Sort the salesytd of each postalcode group in descending order. 
+Shorts the postalcode in ascending order.'
+
+
+'22. From the following table write a query in SQL to count the number of contacts for combination of each type and name. Filter the output for those who have 100 or more contacts. 
+Return ContactTypeID and ContactTypeName and BusinessEntityContact. Sort the result set in descending order on number of contacts.'
+
+select c.contacttypeid, count(b.personid)
+from person.contacttype c
+join Person.BusinessEntityContact b using (contacttypeid)
+group by c.contacttypeid
+having count(b.personid)>100
+order by c.contacttypeid
+
+'23. From the following table write a query in SQL to retrieve the RateChangeDate, full name (first name, middle name and last name) and weekly salary (40 hours in a week) of employees. 
+In the output the RateChangeDate should appears in date format. Sort the output in ascending order on NameInFull.'
+
+SELECT CAST(ratechangedate AS DATE) AS extracted_date, concat(p.lastname,' ',p.firstname) as fullName, 40*rate as weekly_salary
+from HumanResources.EmployeePayHistory e
+join person.person p using(businessentityid)
+order by fullName
+
+'24. From the following tables write a query in SQL to calculate and display the latest weekly salary of each employee. 
+Return RateChangeDate, full name (first name, middle name and last name) and weekly salary (40 hours in a week) of employees Sort the output in ascending order on NameInFull.'
+
+SELECT CAST(ratechangedate AS DATE) AS extracted_date, concat(p.lastname,' ',p.firstname) as fullName, 40*rate as weekly_salary
+from HumanResources.EmployeePayHistory e
+join person.person p using(businessentityid)
+where e.ratechangedate = (select max(ratechangedate) from HumanResources.EmployeePayHistory
+                            where businessentityid = e.businessEntityId)
+order by fullName
+
+
+'25. From the following table write a query in SQL to find the sum, average, count, minimum, and maximum order quentity for those orders whose id are 43659 and 43664. 
+Return SalesOrderID, ProductID, OrderQty, sum, average, count, max, and min order quantity.'
+
+select salesorderid,productid,orderqty,
+sum(orderqty) over win as "Total Quantity",
+avg(orderqty) over win as "Avg Quantity",
+count(orderqty) over win as "No of order",
+min(orderqty) over win as "Min Quantity",
+max(orderqty) over win as "Max Quantity"
+from sales.salesorderdetail
+where salesorderid in (43659,43664)
+window win as (partition by salesorderid)
+
+'26. From the following table write a query in SQL to find the sum, average, and number of order quantity for those orders whose ids are 43659 and 43664 and product id starting with '71'. 
+Return SalesOrderID, OrderNumber,ProductID, OrderQty, sum, average, and number of order quantity.'
+
+select salesorderid,productid,orderqty,
+sum(orderqty) over win as "Total Quantity",
+avg(orderqty) over win as "Avg Quantity",
+count(orderqty) over win as "No of order",
+min(orderqty) over win as "Min Quantity",
+max(orderqty) over win as "Max Quantity"
+from sales.salesorderdetail
+where salesorderid in (43659,43664)
+and CAST(ProductID AS TEXT) LIKE '71%'
+window win as (partition by salesorderid)
+
+'27. From the following table write a query in SQL to retrieve the total cost of each salesorderID that exceeds 100000. Return SalesOrderID, total cost.'
+
+select salesorderid, sum(orderqty*unitprice)
+from Sales.SalesOrderDetail
+group by salesorderid
+having sum(orderqty*unitprice) >100000
+order by salesorderid
+
+'28. From the following table write a query in SQL to retrieve products whose names start with 'Lock Washer'. 
+Return product ID, and name and order the result set in ascending order on product ID column.'
+
+select productid,name
+from Production.Product
+where name like 'Lock Washer%'
+order by productid
+
+'29. Write a query in SQL to fetch rows from product table and order the result set on an unspecified column listprice. Return product ID, name, and color of the product.'
+
+select productid,name,color
+from Production.Product
+order by listprice
+
+
+'30. From the following table write a query in SQL to retrieve records of employees. 
+Order the output on year (default ascending order) of hiredate. Return BusinessEntityID, JobTitle, and HireDate.'
+
+SELECT BusinessEntityID, JobTitle, HireDate  
+FROM HumanResources.Employee  
+ORDER BY DATE_PART('year',HireDate);
+
+
 'rollup
 state,city
 state
